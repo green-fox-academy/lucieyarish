e6c186f93cedc9252bd2c2706a3094ade2e295f1
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 17:39:16 +0200

    Add SecondsInADay exercise.

---
+++ week-01/day-3/SecondsInADay/SecondsInADay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SecondsInADay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 14;
+            int currentMinutes = 34;
+            int currentSeconds = 42;
+
+            // Write a program that prints the remaining seconds (as an integer) from a
+            // day if the current time is represented by the variables above
+            int secondsInMinute = 60;
+            int secondsInHour = 3600;
+            int secondsInDay = 3600 * 24;
+
+            int remainingSeconds = secondsInDay - (currentHours * secondsInHour) - (currentMinutes * secondsInMinute) - 42;
+
+            Console.WriteLine(remainingSeconds);
+        }
+    }
+}
