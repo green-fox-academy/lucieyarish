1e9f130170b48917b35eb219d4bc6d359ea00eee
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 13:52:11 +0200

    Add HelloOthers exercise.

---
+++ week-01/day-3/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Honza!");
+            Console.WriteLine("Hello Adam!");
+            Console.WriteLine("Hello Jakub!");
+        }
+    }
+}
