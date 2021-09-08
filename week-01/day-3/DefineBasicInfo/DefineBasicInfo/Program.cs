d219625a58fdbfbf36290c0d822e4a882eac85ae
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 15:18:07 +0200

    Add BasicInfo exercise.

---
+++ week-01/day-3/DefineBasicInfo/DefineBasicInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefineBasicInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable then print their values
+            // Your name as a string
+            string myName = "Lucie Yarish";
+
+            // Your age as an integer
+            int myAge = 29;
+
+            // Your height in meters as a double
+            double myHeight = 1.6;
+
+            // Whether you are married or not as a boolean
+            bool isMarried = true;
+
+            Console.WriteLine(myName);
+            Console.WriteLine(myAge);
+            Console.WriteLine(myHeight);
+            Console.WriteLine(isMarried);
+        }
+    }
+}
