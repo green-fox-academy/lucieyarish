49d2fefe3bfff5db65ae6b00407f75eb3b200ef7
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 15:10:12 +0200

    Add FavoriteNumber exercise.

---
+++ week-01/day-3/FavoriteNumber/FavoriteNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FavoriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            int myFavoriteNum = 5;
+
+            // And print it like this: "My favorite number is: 8"
+            Console.WriteLine("My favorite number is " + myFavoriteNum);
+        }
+    }
+}
