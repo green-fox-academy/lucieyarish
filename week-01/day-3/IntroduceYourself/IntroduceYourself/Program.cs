13aea6d60f5e6d282e05b42f0f72b90df19ece15
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 14:09:05 +0200

    Add IntroduceYourself exercise.

---
+++ week-01/day-3/IntroduceYourself/IntroduceYourself/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroduceYourself
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lucie Yarish";
+            int age = 31;
+            double height = 1.6;
+
+            Console.WriteLine("Lucie Yarish");
+            Console.WriteLine("31");
+            Console.WriteLine("1.6");
+        }
+    }
+}
