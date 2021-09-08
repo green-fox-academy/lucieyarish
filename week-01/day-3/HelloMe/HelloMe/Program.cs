ee207bd4dc74eb5443bd925881178d29600a729a
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 13:16:40 +0200

    Add HelloMe exercise.

---
+++ week-01/day-3/HelloMe/HelloMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lucie!");
+        }
+    }
+}
