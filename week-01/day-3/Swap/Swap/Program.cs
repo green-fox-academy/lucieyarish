f7214f6fe6cd77a1f7297f10aa335d0d97c1487a
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-08 15:12:35 +0200

    Add Swap exercise.

---
+++ week-01/day-3/Swap/Swap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the variables
+            int a = 123;
+            int b = 526;
+
+            a = 526;
+            b = 123;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
