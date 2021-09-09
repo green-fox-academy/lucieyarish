56193cdd95d1b54d0b12abd3f586a3f60df03b9b
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-09 11:17:51 +0200

    Add OddEven exercise.

---
+++ week-01/day-4/OddEven/OddEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Write a program that reads a number from the standard input,
+            // then prints "Odd" if the number is odd, or "Even" if it is even.
+
+            Console.WriteLine("Give me a random number, please.");
+            string randomNumber = Console.ReadLine();
+
+            int userInput = Int32.Parse(randomNumber);
+
+            if (userInput % 2 == 0)
+            {
+                Console.WriteLine("This is an even number!");
+            } else
+            {
+                Console.WriteLine("This is an odd number!");
+            }
+
+
+
+            
+
+            
+        }
+    }
+}
