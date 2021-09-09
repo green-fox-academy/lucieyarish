a77918bac864167293bec8853134466cb342e570
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-09 17:33:06 +0200

    Add DrawTriangle exercise.

---
+++ week-01/day-4/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DrawTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            Console.WriteLine("Give me a number, please.");
+            string userInput = Console.ReadLine();
+            int userNumber = Int32.Parse(userInput);
+
+            string charForUser = "*";
+
+            for (int i = 1; i <= userNumber; i++)
+            {
+                if ()
+                {
+                    Console.WriteLine(charForUser + charForUser);
+
+                }
+                
+            } 
+
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+        }
+    }
+}
