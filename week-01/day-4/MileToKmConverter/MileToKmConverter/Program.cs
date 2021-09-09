ffdb02d7604f52db892848d4e3fd15fa86cc0ee9
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-09 11:00:11 +0200

    Add MileToKmConverter exercise.

---
+++ week-01/day-4/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a double that is a distance in miles,
+            // then it converts that value to kilometers and prints it
+            int miles = 2;
+            Console.WriteLine("What is " + miles + " multiplied by two?");
+
+            string UserInput = Console.ReadLine();
+
+            // miles * 1.60934;
+            double UserInputNum = Int32.Parse(UserInput);
+
+            Console.WriteLine("Nice! That is " + UserInputNum * 1.60934 + " in kilometers!");
+        }
+    }
+}
