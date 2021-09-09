3a56c990fb7b4d5a9d8b10c334a387708ba4e7ab
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-09 14:34:01 +0200

    Add IWontCheatOnTheExams exercise.

---
+++ week-01/day-4/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IWontCheatOnTheExams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+            //Solution 1 with while loop:
+            
+            int a = 0;
+            while (a <= 100)
+            {
+                Console.WriteLine("I won't cheat on the exam!");
+                a++;
+            }
+
+            // Solution 2 with for loop:
+            // for (int i = 0; i < 100; i++)
+            // {
+            //    Console.WriteLine("I won't cheat on the exam!");
+            // }
+        }
+    }
+}
