e27ee32056944bbb9c45a30a835f8d0880e100fa
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 17:47:57 +0200

    Add Sum exercise.

---
+++ week-01/day-5/Sum/Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function called `sum()` that returns the sum of
+            // numbers from zero to the given parameter
+        }
+
+        static void sum(int myNum)
+        {
+            for (int i = 0; i < myNum; i++)
+            {
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
