7806c4560a914b5cab8a8d2c141be6a4e6f73b02
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 13:24:23 +0200

    Add ChangeElement exercise.

---
+++ week-01/day-5/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[1, 2, 3, 8, 5, 6]`
+            int[] numbers = { 1, 2, 3, 8, 5, 6 };
+            // - Change the value of the fourth element (8) to 4
+            numbers[3] = 4;
+            // - Print the fourth element
+            Console.WriteLine(numbers[3]);
+        }
+    }
+}
