413b27f99f2fa0376be223b206a48287380124e6
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 14:37:11 +0200

    Add DiagonalMatrix exercise.

---
+++ week-01/day-5/DiagonalMatrix/DiagonalMatrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a two dimensional array dynamically with the following content.
+            //   Note that a two dimensional array is often called matrix if every
+            //   internal array has the same length.
+            //   Use a loop!
+
+            int[,] my2DArr = new int[4, 4];
+           
+
+            for (int row = 0; row < my2DArr.GetLength(0); row++)
+            {
+                for (int column = 0; column < my2DArr.GetLength(1); column++)
+                {
+                    if ( )
+                    {
+                        my2DArr[row, column] = 1;
+                    }
+                    
+                }
+                Console.WriteLine();
+            }    
+
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            //   Its length should depend on a variable
+            //  
+            // - Print this two dimensional array to the output
+            
+        }
+    }
+}
