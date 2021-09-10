8d67cfbc77b71c9e9dc07cc491b84ceb942d1bda
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 14:45:23 +0200

    Add DoubleItems exercise.

---
+++ week-01/day-5/DoubleItems/DoubleItems/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            int[] numbers = { 3, 4, 5, 6, 7 };
+            
+
+            // - Double all the values in the array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int result = numbers[i] * 2;
+                Console.WriteLine(result);
+            }
+                
+        }
+    }
+}
