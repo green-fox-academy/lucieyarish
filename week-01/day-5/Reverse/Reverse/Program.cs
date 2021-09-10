41e4eaf183b48d7716491f9a4a276780593dc862
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 17:05:33 +0200

    Add Reverse exercise.

---
+++ week-01/day-5/Reverse/Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            int[] numbers = { 3, 4, 5, 6, 7 };
+
+
+            // - Reverse the order of the elements of `numbers`
+            for (int i = 0; i < numbers.Length / 2; i++)
+            {
+                
+                Console.WriteLine(numbers[i]);
+            }
+
+            // - Print the elements of the reversed `numbers`
+        }
+    }
+}
