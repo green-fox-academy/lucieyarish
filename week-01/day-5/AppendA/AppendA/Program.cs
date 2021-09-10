6fe9376333f6fed9fca625cce8815dc640b3b83e
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 16:09:07 +0200

    Add AppendA exercise.

---
+++ week-01/day-5/AppendA/AppendA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `animals`
+            //   with the following content:
+            //   `["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+            //     "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"]`
+            //
+            // - Add all elements an `"a"` at the end
+
+            string[] animals = {"koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+                 "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn" };
+
+            for (int i = 0; i < animals.GetLength(0); i++)
+            {
+                Console.Write(animals[i] + "a ");
+            }
+
+            // foreach (string animal in animals {
+            //      Console.WriteLine(animal + "a");
+            // }
+
+        }
+    }
+}
