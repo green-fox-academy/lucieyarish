ffdea9e64080a2aed023c360b7d1b81c786fd98c
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 16:38:16 +0200

    Add SumAll exercise.

---
+++ week-01/day-5/SumAll/SumAll/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            int[] numbers = new int [] { 3, 4, 5, 6, 7 };
+            int sum = 0;
+
+            // - Print the sum of the elements of `numbers`
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i]; 
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
