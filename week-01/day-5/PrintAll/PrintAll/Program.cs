03a37b9d68841c29084f31eb7536b264eda7f580
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 13:38:24 +0200

    Add PrintAll exercise.

---
+++ week-01/day-5/PrintAll/PrintAll/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[4, 5, 6, 7]`
+            int[] numbers = { 4, 5, 6, 7 };
+            // - Print all the elements of `numbers`
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i] + "");
+            }
+        }
+    }
+}
