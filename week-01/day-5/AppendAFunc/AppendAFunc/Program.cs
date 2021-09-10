d51e305d88868b11517fafbb3f5338533c4f9f19
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 17:34:02 +0200

    Add AppendAFunc exercise.

---
+++ week-01/day-5/AppendAFunc/AppendAFunc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AppendAFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `typo` and assign the value `Chinchill` to it
+            string typo = "Chinchill";
+            AppendA(typo);
+            
+        }
+
+        // - Write a function called `AppendA()` that gets a string as an input,
+        //   appends an 'a' character to its end and returns with a string
+        // - Print the result of `AppendA(typo)`
+        static void AppendA(string myString)
+        {
+            Console.WriteLine(myString + "a");
+        }
+    }
+}
