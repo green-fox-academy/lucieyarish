cd32e80f024623219aeba1102433530c5da76c31
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 17:19:48 +0200

    Add Doubling exercise.

---
+++ week-01/day-5/Doubling/Doubling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `baseNumber` and assign the value `123` to it
+            int baseNumber = 123;
+            PrintDoubleNumber(baseNumber);
+
+            
+        }
+
+
+        // - Create a function called `DoubleNumber()` that doubles it's input parameter
+        //   and returns the doubled value
+        static void PrintDoubleNumber(int num1)
+        {
+            // - Print the result of `DoubleNumber(baseNumber)
+            Console.WriteLine(num1 * num1);
+        }
+        
+        
+    }
+}
