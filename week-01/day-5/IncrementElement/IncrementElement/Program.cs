8a3bbb7627bf95f2b2271cfc9ace0eb24478803d
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-10 13:33:50 +0200

    Add IncrementElement exercise.

---
+++ week-01/day-5/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `numbers`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            //- Increment the third element
+            numbers[2]++;
+            //- Print the third element
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}
