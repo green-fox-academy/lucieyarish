792e9102518b919ef7dcc7e550919a96ab2c72e3
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-11 16:18:12 +0200

    Add PrintBigger exercise.

---
+++ week-01/day-4/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Please give me a number.");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me one more number, please.");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            } else
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
