d49f5e0fdf0b1d2150518d2f1e71130b2ffee1b5
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-11 14:37:08 +0200

    Add Cuboid exercise.

---
+++ week-01/day-3/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            double cuboidSideSize = 10;
+
+            double cuboidSurface = (cuboidSideSize * cuboidSideSize) * 6;
+            double cuboidVolume = Math.Pow(cuboidSideSize, 3);
+
+            // Surface Area: 600
+            // Volume: 1000
+
+
+            Console.WriteLine(cuboidSurface);
+            Console.WriteLine(cuboidVolume);
+
+            Console.ReadLine();
+        }
+    }
+}
