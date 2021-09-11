d050f8d6022e821601ab4bbdd0915c9af846aef4
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-11 17:34:42 +0200

    Add DiagonalMatrix execise.

---
+++ week-01/day-4/DrawDiagonal/DrawDiagonal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DrawDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+
+            Console.WriteLine("Give me a number, please.");
+            int userNum = int.Parse(Console.ReadLine());
+            string line = "";
+            string charForUser = "%";
+
+            for (int i = 0; i < userNum; i++)
+            {
+                if (i == userNum || i == 1)
+                {
+                    Console.Write(charForUser);
+                } else
+                {
+
+                }
+            }
+
+
+            // The square should have as many lines as the number was
+        }
+    }
+}
