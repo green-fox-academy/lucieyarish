b3e7195391e0c6425f394d760f0c518e8cf50cac
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-11 15:36:31 +0200

    Add AnimalsAndLegs exercise.

---
+++ week-01/day-4/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            Console.WriteLine("How many chickens does the farmer have?");
+            int chickens = int.Parse(Console.ReadLine());
+
+            // The second represents the number of pigs owned by the farmer
+            Console.WriteLine("How many pigs does the farmer have?");
+            int pigs = int.Parse(Console.ReadLine());
+
+            int chickenLegs = chickens * 2;
+            int pigLegs = pigs * 4;
+            int legsTotal = chickenLegs + pigLegs;
+            // It should print how many legs all the animals have
+            Console.WriteLine("The animals have " + legsTotal + " legs altogether.");
+        }
+    }
+}
