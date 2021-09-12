f5b5d130081dbe4998e2b9c4db9ce98461bfe339
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-12 11:57:10 +0200

    Add Factorial exercise.

---
+++ week-01/day-5/Factorial/Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // - Create a function called `calculateFactorial()`
+            //   that returns the factorial of its input
+            Console.WriteLine(calculateFactorial(6));
+        }
+
+        static int calculateFactorial(int myNum)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= myNum; i++)
+            {
+                result = result * i;
+            }
+
+            return result;
+        }
+    }
+}
