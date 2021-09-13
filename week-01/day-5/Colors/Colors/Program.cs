c712f3611416e68a5a189e745a6910120127c36b
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-13 09:40:23 +0200

    Adjust Reverse exercise code and solve Colors exercise.

--- week-01/day-5/Colors/Colors/Program.cs
+++ week-01/day-5/Colors/Colors/Program.cs
@@ -17,9 +17,9 @@ namespace Colors
 
             string[][] colors = new string[3][];
             {
-                colors[0] = new string { "lime", "forest green", "olive", "pale green", "spring green" },
-                colors[1] = new string { "orange red", "red", "tomato" },
-                colors[2] = new string { "orchid", "violet", "pink", "hot pink" }
+                colors[0] = new string[] { "lime", "forest green", "olive", "pale green", "spring green" };
+                colors[1] = new string[] { "orange red", "red", "tomato" };
+                colors[2] = new string[] { "orchid", "violet", "pink", "hot pink" };
              }
 
 
--- week-01/day-5/Reverse/Reverse/Program.cs
+++ week-01/day-5/Reverse/Reverse/Program.cs
@@ -9,12 +9,21 @@ namespace Reverse
             // - Create an array variable named `numbers`
             //   with the following content: `[3, 4, 5, 6, 7]`
             int[] numbers = { 3, 4, 5, 6, 7 };
+            
 
 
             // - Reverse the order of the elements of `numbers`
             for (int i = 0; i < numbers.Length / 2; i++)
             {
+                
+                int temp1 = numbers[0];
+                
+                numbers[0] = numbers[4];
+                numbers[4] = temp1;
 
+                int temp2 = numbers[1];
+                numbers[1] = numbers[3];
+                numbers[3] = temp2;
                 
                 Console.WriteLine(numbers[i]);
             }
