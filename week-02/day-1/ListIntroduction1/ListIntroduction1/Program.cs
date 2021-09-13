a6b8f160704c064065e91caa94fe7a69b8c6221d
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-13 17:35:39 +0200

    Change ListIntroduction1 exercise and add MapIntroduction1 exercise.

--- week-02/day-1/ListIntroduction1/ListIntroduction1/Program.cs
+++ week-02/day-1/ListIntroduction1/ListIntroduction1/Program.cs
@@ -8,16 +8,25 @@ namespace ListIntroduction1
         static void Main(string[] args)
         {
             List<string> names = new List<string>();
-            names.Add("1. William");
-            names.Add("2. John");
-            names.Add("3. Amanda");
+            names.Add("William");
+            names.Add("John");
+            names.Add("Amanda");
 
             //Iterate through the list and print out each name
 
-            //foreach (string name in names)
-            //{
-            //    Console.WriteLine(name);
-            //}
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Iterate through the list and print
+            int order = 0;
+            foreach (var name in names)
+            {
+                order += 1;
+                Console.WriteLine(order + ". " + name);
+            }
+
 
             //Remove the 2nd element
             names.RemoveAt(1);
@@ -31,7 +40,11 @@ namespace ListIntroduction1
             }
 
             //Console.WriteLine(names);
-            
+
+            //Remove all elements
+            names.Clear();
+            Console.WriteLine(names.Count);
+
         }
     }
 }
---
+++ week-02/day-1/MapIntroduction1/MapIntroduction1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapIntroduction1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, char> myMap = new Dictionary <int, char> {
+                {97,'a'},
+                {98,'b'},
+                {99,'c'},
+                {65,'A'},
+                {66,'B'},
+                {67,'C'},
+            };
+
+            //Print all the keys
+            foreach (var element in myMap)
+            {
+                Console.WriteLine(element.Key);
+            }
+
+            //Print all the values
+            foreach (var element in myMap)
+            {
+                Console.WriteLine(element.Value);
+            }
+
+            //Add value D with the key 68
+            myMap.Add(68, 'D');
+
+            //Print how many key-value pairs are in the map
+            Console.WriteLine(myMap.Count);
+        }
+    }
+}
