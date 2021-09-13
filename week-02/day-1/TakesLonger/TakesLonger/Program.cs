71d82e05837efcb71aad6151d30b323680860dfe
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-13 13:31:25 +0200

    Add TakesLonger exercise.

---
+++ week-02/day-1/TakesLonger/TakesLonger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TakesLonger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, " +
+                "even when you take into account Hofstadter's Law.";
+
+            // When saving this quote a disk error has occured. Please fix it!
+            // Insert the words "always takes longer than" between the words
+            // "It" and "you" using the StringBuilder class!
+            // Use pieces of the quote variable
+            // (instead of just redefining the string)!
+
+
+            StringBuilder sb = new StringBuilder(quote);
+
+            string insertWords = "always takes longer than ";
+
+            int index1 = quote.IndexOf("you");
+
+            sb.Insert(index1, insertWords);
+
+
+
+
+
+
+
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ week-02/day-1/UrlFixer/UrlFixer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UrlFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+
+            // Accidentally I got the wrong URL for a funny subreddit.
+            // It's probably "odds" and not "bots"
+            // Also, the URL is missing a crucial component,
+            // find out what it is and insert it too!
+            // Try to solve it more than once using different
+            // string functions!
+
+            //Adding colon:
+            url = url.Insert(5, ":");
+
+            // Change part of string with .Replace
+            url = url.Replace("bots", "odds");
+
+            Console.WriteLine(url);
+        }
+    }
+}
