8d81aa0eb5182601ee8a385d536c4d73d825c295
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-14 15:50:59 +0200

    Add WriteSingleLine exercise.

---
+++ week-02/day-2/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that is able to manipulate a file
+            // By writing your name into it as a single line
+            // The file should be named "my-file.txt"
+            // In case the program is unable to write the file,
+            // It should print the following error message:
+            // "Unable to write file: my-file.txt"
+
+            string path = "Assest/my-file.txt";
+            WriteLinesFunction(path);
+            
+
+            static void WriteLinesFunction (string path)
+            {
+                try
+                {
+                    File.WriteAllLines(path, new string[] { "Lucie Yarish" });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unable to write file: my-file.txt");
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
