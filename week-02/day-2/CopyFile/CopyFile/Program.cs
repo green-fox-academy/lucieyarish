7cb0bf948c3ba551b6fe3dbfaf5b8ab8b2258184
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-14 17:37:49 +0200

    Add CopyFile exercise.

---
+++ week-02/day-2/CopyFile/CopyFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = "myFile.txt";
+            string path2 = "myFile2.txt";
+            CopyFile(path1, path2);
+        }
+        // Write a function that reads all lines of a file and writes
+        // the read lines to an other file (a.k.a copies the file)
+        // It should take the filenames as parameters
+        // It should return a boolean that shows if the copy was successful
+        static string CopyFile(string copyFrom, string copyTo)
+        { 
+            try
+            {
+
+                string[] file1 = File.ReadAllLines(copyFrom);
+                // take content of file 1 and copy to a new file
+                File.WriteAllLines(copyTo, file1);
+                
+            }
+            catch (Exception ex)
+            {
+	            
+            }
+            return "";
+        }
+    }
+}
