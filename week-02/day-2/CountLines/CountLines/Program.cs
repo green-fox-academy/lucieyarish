ea39b39af496514ff9bb8335cfb11ad351684763
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-14 14:27:26 +0200

    Add CountLines exercise.

---
+++ week-02/day-2/CountLines/CountLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string path = "Assets/myFile.txt";
+            LinesCount(path);
+   
+       
+
+        }
+
+        static void LinesCount(string path)
+        {
+            
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                int lines = content.Length;
+                Console.WriteLine(lines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("0");
+            }
+        }
+
+    }
+
+
+
+}
