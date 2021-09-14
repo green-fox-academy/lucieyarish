ffcc18ba3905a7b0ce1401774f0a8fc407edce6b
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-14 11:59:05 +0200

    Add PrintEachLine exercise.

---
+++ week-02/day-2/PrintEachLine/PrintEachLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that opens a file called "my-file.txt",
+            // then prints
+            // each line from the file.
+            // If the program is unable to read the file (for example
+            // it does not exist),
+            // then it should print the following error message:
+            // "Unable to read file: my-file.txt"
+            string path = @"my-file.txt";
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+            }
+            catch (Exception ex)
+            {
+                    Console.WriteLine("Unable to read file: my-file.txt");
+            }
+        }   
+    }
+}
