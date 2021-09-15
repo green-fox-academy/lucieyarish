142c9925d6904d01d865b9fd81e12159d82e8a7a
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-15 11:23:03 +0200

    Add Doubling exercise.

--- week-01/day-5/Reverse/Reverse/Program.cs
+++ week-01/day-5/Reverse/Reverse/Program.cs
@@ -19,6 +19,8 @@ namespace Reverse
                 Console.WriteLine(numbers[i]);
             }
 
+
+
             
         }
     }
---
+++ week-02/day-2/Doubled/Doubled/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace Doubled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+        }
+
+        static string DecryptFile(string file)
+        {
+            string path = "duplicated-chars.txt";
+            string[] fileContent = File.ReadAllLines(path);
+            foreach (var item in fileContent)
+            {
+                Console.WriteLine(item);
+            }
+ 
+        }
+    }
+}
