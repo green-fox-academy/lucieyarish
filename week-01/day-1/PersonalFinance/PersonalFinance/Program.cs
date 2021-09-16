85a5b2d7c7deff211b34b175dec5b1cffc3e8280
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-16 16:51:39 +0200

    Add PersonalFinance exercise.

---
+++ week-01/day-1/PersonalFinance/PersonalFinance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonalFinance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> myPersonalFinance = new Dictionary<int, int> {
+                {1,500},
+                {2,1000},
+                {3,1250},
+                {4,175},
+                {5,800},
+                {6,120},
+            };
+
+            foreach (var item in myPersonalFinance)
+            {
+                
+            }
+          
+        }
+    }
+}
