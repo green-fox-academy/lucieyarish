ba0af046dda1e9a587e34e262590c9f8370478da
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-17 11:30:42 +0200

    Add NumberAdder exercise.

---
+++ week-02/day-5/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a recursive function that takes one parameter:
+            //n and adds numbers from 1 to n.
+            Console.WriteLine(AddWithRecursion(3));
+        }
+
+        private static int AddWithRecursion(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + AddWithRecursion(n - 1);
+            }
+        }
+    }
+}
