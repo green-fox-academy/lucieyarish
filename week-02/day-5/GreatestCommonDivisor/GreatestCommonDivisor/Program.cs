c61c45ce62e2977367c7f920018234ce51196f86
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-17 17:41:43 +0200

    Add unfinished GreatestCommonDivisor exercise.

---
+++ week-02/day-5/GreatestCommonDivisor/GreatestCommonDivisor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreatestCommonDivisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find the greatest common divisor of two numbers using recursion.
+        }
+    }
+}
