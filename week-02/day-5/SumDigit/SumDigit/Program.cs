cfa6c113cd082e66a01b0c2c26bf9c1be67fb98e
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-17 14:42:59 +0200

    Add SumDigit with recursion exercise.

---
+++ week-02/day-5/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given a non - negative integer n, return the sum
+            //of its digits recursively(without loops).
+            Console.WriteLine(SumDigits(123.ToString()));
+        }
+
+        private static int SumDigits(string numToDigits)
+        {
+            int firstNumber = int.Parse(numToDigits.Substring(0, 1));
+
+            if (numToDigits.Length <= 1)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return firstNumber + (SumDigits(numToDigits.Substring(1)));
+            }
+        }
+    }
+}
