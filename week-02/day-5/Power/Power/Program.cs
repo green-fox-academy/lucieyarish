a56ef0375cf567dacf2de1c3e7618a915fa29260
lucieyarish <79669599+lucieyarish@users.noreply.github.com>
2021-09-17 15:29:41 +0200

    Add Power with recursion exercise.

---
+++ week-02/day-5/Power/Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given base and n that are both 1 or more, compute recursively(no loops)
+            //the value of base to the n power, so powerN(3, 2) is 9(3 squared).
+
+            Console.WriteLine(PowerWithRecursion(3, 2));
+        }
+
+        private static int PowerWithRecursion(int baseNum, int powerN)
+        {
+            if (baseNum == 1 || powerN == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return baseNum * PowerWithRecursion(baseNum, powerN - 1);
+            }
+        }
+    }
+}
