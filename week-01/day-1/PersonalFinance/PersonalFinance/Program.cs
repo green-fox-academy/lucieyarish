af39e57d5aa06ef46294430eb34474074095f739
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-19 18:06:28 +0200

    Add PersonalFinance exercise.

--- week-01/day-1/PersonalFinance/PersonalFinance/Program.cs
+++ week-01/day-1/PersonalFinance/PersonalFinance/Program.cs
@@ -7,20 +7,48 @@ namespace PersonalFinance
     {
         static void Main(string[] args)
         {
-            Dictionary<int, int> myPersonalFinance = new Dictionary<int, int> {
-                {1,500},
-                {2,1000},
-                {3,1250},
-                {4,175},
-                {5,800},
-                {6,120},
-            };
+            List<int> myPersonalFinance = new List<int>();
+            myPersonalFinance.AddRange(new int[] { 500, 1000, 1250, 175, 800, 120 });
 
+            int sum = 0;
             foreach (var item in myPersonalFinance)
             {
-                
+                sum += item;
             }
-          
+
+            Console.WriteLine($"You've spent {sum} in total.");
+
+            int greatestExpense = 0;
+            foreach (int item in myPersonalFinance)
+            {
+                if (greatestExpense < item)
+                {
+                    greatestExpense = item;
+                }
+            }
+
+            Console.WriteLine($"The greatest expense was: { greatestExpense}.");
+
+            int lowestExpense = 0;
+            foreach (int item in myPersonalFinance)
+            {
+                if (lowestExpense < item)
+                {
+                    lowestExpense = item;
+                }
+            }
+
+            Console.WriteLine($"The lowest expense was: { lowestExpense}.");
+
+            int averageSpent = 0;
+            foreach (int item in myPersonalFinance)
+            {
+                averageSpent += item;
+            }
+
+            averageSpent = averageSpent / myPersonalFinance.Count;
+
+            Console.WriteLine($"The average amount of your spendings: { averageSpent }.");
         }
     }
 }
