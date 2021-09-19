7d0c9ba44519337601586bec7db22591dca00ec9
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-19 20:18:00 +0200

    Add unfinished DrawPyramid exercise.

---
+++ week-01/day-4/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DrawPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number.");
+            int userNum = int.Parse(Console.ReadLine());
+
+            int[,] pyramid = new int[userNum, 10];
+            int numForUser = 1;
+
+            for (int row = 0; row < pyramid.GetLength(0); row++)
+            {
+                for (int column = 0; column < pyramid.GetLength(1); column++)
+                {
+                    Console.WriteLine(pyramid[row,column]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- week-02/day-2/Doubled/Doubled/Program.cs
+++ week-02/day-2/Doubled/Doubled/Program.cs
@@ -8,19 +8,24 @@ namespace Doubled
     {
         static void Main(string[] args)
         {
-         
+            string path = "duplicated-chars.txt";
+            DecryptFile(path);
             
         }
 
         static string DecryptFile(string file)
         {
+
             string path = "duplicated-chars.txt";
             string[] fileContent = File.ReadAllLines(path);
             foreach (var item in fileContent)
             {
                 Console.WriteLine(item);
             }
- 
+
+            
+
+            
         }
     }
 }
