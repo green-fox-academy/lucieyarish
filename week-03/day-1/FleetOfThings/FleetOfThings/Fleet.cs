d54b4b5ce429ead8bf270decf94b300d59ff161a
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-20 20:32:24 +0200

    Add FleetOfThings exercise.

---
+++ week-03/day-1/FleetOfThings/FleetOfThings/Fleet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace FleetOfThings
+{
+    public class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+
+        public void PrintAll()
+        {
+            for (int i = 0; i < Things.Count; i++)
+            {
+                if (Things[i].Completed == true)
+                {
+                    Console.WriteLine($"{i + 1}. [x] {Things[i].Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{i + 1}. [ ] {Things[i].Name}");
+                }
+            }
+        }
+    }
+}
---
+++ week-03/day-1/FleetOfThings/FleetOfThings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FleetOfThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fleet = new Fleet();
+            Thing item1 = new Thing("milk");
+            Thing item2 = new Thing("Remove the obstacles");
+            Thing item3 = new Thing("Stand up");
+            Thing item4 = new Thing("Eat lunch!");
+            item3.Complete();
+            item4.Complete();
+
+            fleet.Add(item1);
+            fleet.Add(item2);
+            fleet.Add(item3);
+            fleet.Add(item4);
+            fleet.PrintAll();
+
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+        }
+    }
+}
