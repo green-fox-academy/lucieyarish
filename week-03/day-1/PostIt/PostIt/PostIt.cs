729f8b4f41723954372b731350c2d7c4915c2f5e
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-20 11:51:21 +0200

    Add PostIt exercise.

---
+++ week-03/day-1/PostIt/PostIt/PostIt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PostIt
+{
+    public class PostIt
+    {
+        private string BackgroundColor;
+        private string Text;
+        private string TextColor;
+
+        public PostIt(string backgroundColor, string text, string textColor)
+        {
+            BackgroundColor = backgroundColor;
+            Text = text;
+            TextColor = textColor;
+        }
+
+        public void PrintPostItColor()
+        {
+            Console.WriteLine($"My background is { BackgroundColor }, my text is { TextColor }, and I say { Text }");
+        }
+    }
+}
---
+++ week-03/day-1/PostIt/PostIt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostIt postIt1 = new PostIt("orange", "Idea 1", "blue");
+            postIt1.PrintPostItColor();
+
+            PostIt postIt2 = new PostIt("pink", "Awesome", "black");
+            postIt2.PrintPostItColor();
+
+            PostIt postIt3 = new PostIt("yellow", "Superb!", "green");
+            postIt3.PrintPostItColor();
+        }
+    }
+}
