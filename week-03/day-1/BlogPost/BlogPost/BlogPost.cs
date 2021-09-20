c32e96d656615a461b26d714144baf10400da3fa
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-20 12:08:15 +0200

    Add BlogPost exercise.

---
+++ week-03/day-1/BlogPost/BlogPost/BlogPost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace BlogPost
+{
+    public class BlogPost
+    {
+        private string AuthorName;
+        private string Title;
+        private string Text;
+        private string PublicationDate;
+
+        public BlogPost(string authorname, string title, string text, string publicationdate)
+        {
+            AuthorName = authorname;
+            Title = title;
+            Text = text;
+            PublicationDate = publicationdate;
+        }
+
+        public void CreateBlogPost()
+        {
+            Console.WriteLine($"'{Title}' written by {AuthorName} published on {PublicationDate}. {Text}");
+        }
+    }
+}
