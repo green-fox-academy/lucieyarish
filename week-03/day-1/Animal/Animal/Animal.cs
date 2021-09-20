a9c3ee4c60e811da180a941a6e85e8b605d5f10e
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-20 13:35:13 +0200

    Add Animals exercise.

---
+++ week-03/day-1/Animal/Animal/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Animal
+{
+    public class Animal
+    {
+        private int Hunger;
+        private int Thirst;
+
+        public Animal(int hunger, int thirst)
+        {
+            Hunger = hunger;
+            Thirst = thirst;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+            Console.WriteLine(Hunger);
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+            Console.WriteLine(Thirst);
+        }
+
+        public void Play()
+        {
+
+            Hunger+= 1;
+            Thirst+= 1;
+            Console.WriteLine(Hunger + " and " + Thirst);
+        }
+    }
+}
---
+++ week-03/day-1/Animal/Animal/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal crocodile = new Animal(50, 50);
+            crocodile.Eat();
+            crocodile.Drink();
+            crocodile.Play();
+        }
+    }
+}
