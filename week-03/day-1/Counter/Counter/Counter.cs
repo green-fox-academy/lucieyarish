91667e0a6f8690c1582f6539e3727cb1be478f8f
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-20 14:43:22 +0200

    Add Counter exercise.

---
+++ week-03/day-1/Counter/Counter/Counter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Counter
+{
+    public class Counter
+    {
+        public int CounterProp { get; set; }
+
+        public Counter()
+        {
+            CounterProp = 0;
+        }
+
+        public Counter(int addnumber)
+        {
+            CounterProp = addnumber;
+        }
+
+        public void Add(int number)
+        {
+            CounterProp += number;
+            Console.WriteLine($"Counter value is: {CounterProp}");
+        }
+
+        public void Add()
+        {
+            CounterProp++;
+            Console.WriteLine($"Counter value is: {CounterProp}");
+        }
+
+        public void Get()
+        {
+            Console.WriteLine($"Counter value is: {CounterProp}");
+        }
+
+        public void Reset()
+        {
+            CounterProp = 0;
+            Console.WriteLine(CounterProp);
+        }
+
+    }
+}
---
+++ week-03/day-1/Counter/Counter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter myCounter = new Counter();
+            myCounter.Add();
+            myCounter.Add(2);
+            myCounter.Get();
+            myCounter.Reset();
+        }
+    }
+}
