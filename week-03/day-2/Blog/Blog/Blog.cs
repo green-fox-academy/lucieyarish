d8bbf5649698f35f5554d9d204223409d12f3537
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-21 22:28:53 +0200

    Add unfinished Blog exercise.

---
+++ week-03/day-2/Blog/Blog/Blog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blog
+{
+    public class Blog
+    {
+        private List<BlogPost> MyBlog;
+
+        public Blog()
+        {
+            MyBlog = new List<BlogPost>();
+        }
+
+        public void Add(BlogPost post)
+        {
+            MyBlog.Add(post);
+        }
+
+        public void Delete(int i)
+        {
+            for (int j = 0; j < MyBlog.Count; j++)
+            {
+                MyBlog.RemoveAt(i);
+            }
+        }
+
+        public void Update(int i, BlogPost post)
+        {
+
+        }
+    }
+}
---
+++ week-03/day-2/Blog/Blog/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blog
+{
+    public class BlogPost
+    {
+        private string AuthorName;
+        private string Title;
+        private string Text;
+        private string PublicationDate;
+
+        public BlogPost(string authorname, string title, string text, string publicationdate)
+        {
+            AuthorName = authorname;
+            Title = title;
+            Text = text;
+            PublicationDate = publicationdate;
+        }
+
+        public void CreateBlogPost()
+        {
+            Console.WriteLine($"'{Title}' written by {AuthorName} published on {PublicationDate}. {Text}");
+        }
+    }
+}
