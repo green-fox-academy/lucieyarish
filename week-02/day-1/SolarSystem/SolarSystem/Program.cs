467fdd8d29b5fcbc6dff4c5d29e0f2887f519705
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-26 22:47:40 +0200

    Add week 2 Lists SolarSystem exercise.

---
+++ week-02/day-1/SolarSystem/SolarSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+            planetList.Insert(5, "Saturn");
+            
+            foreach (var planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+            Console.ReadLine();
+        }
+    }
+}
