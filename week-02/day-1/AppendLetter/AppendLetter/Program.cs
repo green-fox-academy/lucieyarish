f8ae27e20366ea0b4fcb011b5b11a5183fb90bbb
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-28 11:06:17 +0200

    Add lists AppendLetter exercise.

---
+++ week-02/day-1/AppendLetter/AppendLetter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppendLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string> {"koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill", "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"};
+            AppendA(animals);
+        }
+
+        public static void AppendA(List<string> listName)
+        {
+            foreach (string item in listName)
+            {
+                Console.WriteLine(item + "a");
+            }
+        }
+    }
+}
