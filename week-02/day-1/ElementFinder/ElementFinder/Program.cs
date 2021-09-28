dd5ee7175fee2b4fa61fe32488c086ddb4298330
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-28 11:15:30 +0200

    Add lists ElementFinder exercise.

---
+++ week-02/day-1/ElementFinder/ElementFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElementFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var List = new List<int>{1, 2, 3, 4, 5};
+            ContainsSeven(List);
+        }
+
+        public static void ContainsSeven(List<int> listName)
+        {
+            if (listName.Contains(7))
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Noooooooo");
+            }
+        }
+    }
+}
