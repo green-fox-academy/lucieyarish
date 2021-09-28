e2bb4e278595c4829d4427be8dd0393b5d53d2f7
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-28 11:46:31 +0200

    Add lists ShoppingList exercise.

---
+++ week-02/day-1/ShoppinList/ShoppinList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppinList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = new List<string>{"eggs", "milk", "fish", "apples", "bread", "chicken"};
+            DoWeHaveMilk(shoppingList);
+            DoWeHaveBananas(shoppingList);
+        }
+        
+        private static void DoWeHaveMilk(List<string> shoppingList)
+        {
+            if (shoppingList.Contains("milk"))
+            {
+                Console.WriteLine("We do have milk!");
+            }
+            else
+            {
+                Console.WriteLine("No milk!");
+            }
+        }
+        
+        private static void DoWeHaveBananas(List<string> shoppingList)
+        {
+            if (shoppingList.Contains("bananas"))
+            {
+                Console.WriteLine("We do have bananas!");
+            }
+            else
+            {
+                Console.WriteLine("No bananas!");
+            }
+        }
+    }
+}
