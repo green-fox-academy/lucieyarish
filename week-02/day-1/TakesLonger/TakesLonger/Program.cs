b2faebc8e2c2891079d4bbcac138730aeac1d555
Lucie Yarish <lucieyarish@hotmail.com>
2021-09-28 11:29:48 +0200

    Adjust strings TakesLonger exercise.

--- week-02/day-1/TakesLonger/TakesLonger/Program.cs
+++ week-02/day-1/TakesLonger/TakesLonger/Program.cs
@@ -8,28 +8,16 @@ namespace TakesLonger
     {
         static void Main(string[] args)
         {
-            string quote = "Hofstadter's Law: It you expect, " +
-                "even when you take into account Hofstadter's Law.";
-
-            // When saving this quote a disk error has occured. Please fix it!
-            // Insert the words "always takes longer than" between the words
-            // "It" and "you" using the StringBuilder class!
-            // Use pieces of the quote variable
-            // (instead of just redefining the string)!
-
-
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
             StringBuilder sb = new StringBuilder(quote);
-
             string insertWords = "always takes longer than ";
-
-            int index1 = quote.IndexOf("you");
-
+            int index1 = quote.IndexOf("you"); 
             sb.Insert(index1, insertWords);
-
-
-
-
             Console.WriteLine(sb);
+            
+            //StringBuilder sb = new StringBuilder(quote);
+            //sb.Insert(21, "always takes longer than");
+            //Console.WriteLine(sb);
         }
     }
 }
