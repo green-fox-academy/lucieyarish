e4485da69ec1b1989f2d42565b94791ff713c752
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-06 22:17:02 +0200

    Add TrickyAverage exercise.

---
+++ week-05/day-4/TrickyAverage/TrickyAverage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TrickyAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 3, 4, 5, 6};
+            int[] numbers2 = { 5, 2, 8, -1};
+
+            Console.WriteLine(GetTrickyAvg(numbers2));
+        }
+
+        public static double GetTrickyAvg(int[] numbers)
+        {
+            double currentLargestEven = 0;
+            foreach (var item in numbers)
+            {
+                if (item % 2 == 0 && item > currentLargestEven)
+                {
+                    currentLargestEven = item;
+                }
+            }
+
+            double currentSmallestOdd = 0;
+            foreach (var item in numbers)
+            {
+                if (item < currentSmallestOdd)
+                {
+                    currentSmallestOdd = item;
+                }
+            }
+
+            double trickyAverage = (currentLargestEven + currentSmallestOdd) / 2;
+            
+            return trickyAverage;
+        }
+    }
+}
