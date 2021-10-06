76da19bafa22083f34d671db415b6427ae6df7f8
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-06 14:00:00 +0200

    Add completed MutualElements exercise.

---
+++ week-05/day-3/MutualElements/MutualElements/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MutualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfNums1 = new List<int>
+            {
+                1, 1, 2, 3, 3, 4
+            };
+            
+            List<int> listOfNums2 = new List<int>
+            {
+                3, 4, 4, 5, 6
+            };
+            
+            GetMutualElements(listOfNums1, listOfNums2);
+        }
+
+        private static void DisplayMutualValues(List<int> mutualElements)
+        {
+            foreach (var item in mutualElements)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static List<int> GetMutualElements(List<int> listOfNums1, List<int> listOfNums2)
+        {
+            List<int> mutualElements = new List<int>();
+
+            foreach (int item in listOfNums1)
+            {
+                if (listOfNums2.Contains(item) && !mutualElements.Contains(item))
+                {
+                    mutualElements.Add(item);
+                }
+            }
+            foreach (var item in listOfNums2)
+            {
+                if (listOfNums1.Contains(item) && !mutualElements.Contains(item))
+                {
+                    mutualElements.Add(item);
+                }
+            }
+            
+            DisplayMutualValues(mutualElements);
+            return mutualElements;
+        }
+        
+        
+    }
+}
