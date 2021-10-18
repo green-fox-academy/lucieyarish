40c5fc9c9ad5db7d4cc277959b32e0744d8c0c9f
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-18 17:33:17 +0200

    Add unfinished Exercise12

---
+++ week-06/day-1/LINQExercise12/LINQExercise12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LINQExercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "starwars.txt";
+
+            var printHeaviestCharacter =
+                File.ReadAllLines(filePath)
+                    .OrderByDescending(s => ParsingMethod(s.Split(';')[2]))
+                    .Take(1);
+
+            foreach (var item in printHeaviestCharacter)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+        public static double ParsingMethod(string s)
+        {
+            double numVal;
+            try
+            {
+                numVal = Double.Parse(s);
+            }
+            catch (Exception e)
+            {
+                numVal = 0;
+            }
+            return numVal;
+        }
+    }
+}
