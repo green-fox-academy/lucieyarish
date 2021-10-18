393b958655b71e75333c127c618d5e066c50c321
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-18 11:54:32 +0200

    Add Exercise6

---
+++ week-06/day-1/LINQExercise6/LINQExercise6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "aaabb";
+
+            var findFrequencyOfCharsLambda = myString.ToCharArray().GroupBy(ch => ch).ToDictionary(ch => ch.Key, ch => ch.Count());
+            var findFrequencyOfCharsQuery = from character in myString.ToCharArray()
+                group character by character
+                into characters
+                select new { characters.Key, Count = characters.Count()};
+
+
+            foreach (var item in findFrequencyOfCharsQuery)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ week-06/day-1/LINQExercise7/LINQExercise7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var getCityWithLambda = cities.GroupBy(c => c).To
+        }
+    }
+}
