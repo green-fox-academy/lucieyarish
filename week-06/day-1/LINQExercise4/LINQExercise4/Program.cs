420f2a3f534f9658500b67f07a0d1198f7bb52dc
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-18 11:37:16 +0200

    Add LING Exercise4 and Exercise5

---
+++ week-06/day-1/LINQExercise4/LINQExercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new[] { 3, 9, 2, 8, 6, 5 };
+
+            var getSquaredWithLambda = numbers.Where(n => (n * n) > 20).ToList();
+            var getSquaredWithQuery = from number in numbers
+                where (number * number) > 20
+                select number;
+        }
+    }
+}
---
+++ week-06/day-1/LINQExercise5/LINQExercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQExercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var getValueFrequencyWithLambda = numbers.GroupBy(n => n).ToDictionary(n => n.Key, n => n.Count());
+            var getValueFrequencyWithQuery = from number in numbers
+                group number by number
+                into g
+                select new {g.Key, Count = g.Count()};
+
+            foreach (var number in getValueFrequencyWithLambda)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
