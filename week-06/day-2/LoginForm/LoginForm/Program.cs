f442c216d9400de54cbf0ba97d0d63fcbcc21247
Lucie Yarish <lucieyarish@hotmail.com>
2021-10-19 13:28:36 +0200

    Add LoginForm solution

---
+++ week-06/day-2/LoginForm/LoginForm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoginForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
