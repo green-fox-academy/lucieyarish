7600220e2ed10690e5154e3e6881cd6f4508e0a3
Lucie Yarish <lucieyarish@hotmail.com>
2021-11-10 22:06:08 +0100

    Add Bootstrap and start styling Index page

--- week-08/day-2/ListingTodos/ListingTodos/Startup.cs
+++ week-08/day-2/ListingTodos/ListingTodos/Startup.cs
@@ -30,6 +30,7 @@ namespace ListingTodos
             services.AddTransient<TodoService>();
             services.AddTransient<AssigneeService>();
             ConfigureDb(services);
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
