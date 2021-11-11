15ac314318b36441457c9bef8a02c74e67a4e754
Lucie Yarish <lucieyarish@hotmail.com>
2021-11-11 14:12:00 +0100

    Create basic view with form

--- week-09/day-4/URLAliaser/URLAliaser/Controllers/HomeController.cs
+++ week-09/day-4/URLAliaser/URLAliaser/Controllers/HomeController.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using URLAliaser.Services;
 
 namespace URLAliaser.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
+        private LinkService LinkService { get; }
+
+        public HomeController(LinkService service)
+        {
+            LinkService = service;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
         
     }
 }
---
+++ week-09/day-4/URLAliaser/URLAliaser/Migrations/20211111130110_init.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace URLAliaser.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    LinkURL = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Alias = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
